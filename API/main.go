845cc960892c6b68b19c428c9fdae1f9e41f7906
VagueCoder <vaguecoder0to.n@gmail.com>
2021-01-04 07:10:59 +0530

    Enhancements

--- API/main.go
+++ API/main.go
@@ -7,23 +7,47 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
+	// "os"
 	"time"
 
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type DatabaseObj struct {
-	Collection *mongo.Collection
+type DatabaseObject struct {
+	Collection	*mongo.Collection
+	Context		context.Context
 }
 
-func homePage(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "Welcome Page")
+func (db DatabaseObject) GetEndpoint(writer http.ResponseWriter, request *http.Request) {
+	var data []map[string]interface{}
+
+	cursor, err := db.Collection.Find(db.Context, bson.M{})
+	if err != nil {
+		writer.WriteHeader(http.StatusInternalServerError)
+		writer.Write([]byte(`{"message": "` + err.Error() + `"}`))
+		return
+	}
+	defer cursor.Close(db.Context)
+
+	for cursor.Next(db.Context) {
+		var doc map[string]interface{}
+		cursor.Decode(&doc)
+		data = append(data, doc)
+	}
+
+	if err := cursor.Err(); err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			writer.Write([]byte(`{"message": "` + err.Error() + `"}`))
+			return
+	}
+
+	json.NewEncoder(writer).Encode(data)
 }
 
-func (db DatabaseObj) rootEndpoint(writer http.ResponseWriter, request *http.Request) {
+func (db DatabaseObject) PostEndpoint(writer http.ResponseWriter, request *http.Request) {
 	var data interface{}
 	byteData, err := ioutil.ReadAll(request.Body)
 	if err != nil {
@@ -39,9 +63,8 @@ func (db DatabaseObj) rootEndpoint(writer http.ResponseWriter, request *http.Req
 	if err != nil {
 		log.Fatal("Insert Error: ", err)
 	}
-	fmt.Fprintf(writer, "\n\n%+v", status)
-
-	fmt.Fprintf(writer, "\n\n%+v", data)
+	
+	json.NewEncoder(writer).Encode(status)
 }
 
 func main() {
@@ -62,13 +85,13 @@ func main() {
 	}
 	defer client.Disconnect(ctx)
 
-	databaseObj := DatabaseObj{
+	dbo := DatabaseObject{
 		Collection: client.Database("Mydatabase").Collection("Mycollection"),
 	}
 
 	router := mux.NewRouter()
-	router.HandleFunc("/", homePage).Methods("GET")
-	router.HandleFunc("/", databaseObj.rootEndpoint).Methods("POST")
+	router.HandleFunc("/", dbo.GetEndpoint).Methods("GET")
+	router.HandleFunc("/", dbo.PostEndpoint).Methods("POST")
 	fmt.Println("RESTful-API-Go-Endpoint Hosted on port " + port)
 	log.Fatal("HTTP Error: ", http.ListenAndServe(port, router))
 }
